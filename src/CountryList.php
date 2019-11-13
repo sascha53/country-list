c0180a46b522f3451f87cabcbd95fbbd922b4885
Janusz Szczypka <janusz@fide.pl>
2019-11-13 13:33:00 +0100

    first version

--- src/CountryList.php
+++ src/CountryList.php
@@ -7,7 +7,13 @@ abstract class CountryList
 
     static function get(string $language): array
     {
-	die(__DIR__);
+	$path = __DIR__.'/../data/'.$language;
+
+	if(!is_dir($path)) {
+	    throw new \Exception("Language $language not found.");
+	}
+
+	return require $path.'/country.php';
     }
 
 }
